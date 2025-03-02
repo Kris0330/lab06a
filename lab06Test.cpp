45bc734787b73ec2305830a5897941c1b7a9b7bb
Yuan Chun CHENG <yuanchun_cheng@ucsb.edu>
2025-03-02 08:47:08 -0800

    Update lab06Test.cpp

--- lab06Test.cpp
+++ lab06Test.cpp
@@ -5,14 +5,23 @@
 int main() {
     WordCount wc;
 
-    wc.incrWordCount("apple");
-    wc.incrWordCount("Apple"); 
-    wc.incrWordCount("banana");
+    ASSERT_EQUALS("isn't", wc.makeValidWord("isn't"));
+    ASSERT_EQUALS("mother-in-law", wc.makeValidWord("mother-in-law"));
+    ASSERT_EQUALS("apple", wc.makeValidWord("...apple***"));
+    ASSERT_EQUALS("foo-bar", wc.makeValidWord("foo-bar-"));
+    ASSERT_EQUALS("", wc.makeValidWord("-$%#"));
 
-    ASSERT_EQUALS(2, wc.getWordCount("apple"));
-    ASSERT_EQUALS(1, wc.getWordCount("banana"));
-    ASSERT_EQUALS(3, wc.getTotalWords());
-    ASSERT_EQUALS(2, wc.getNumUniqueWords());
+    wc.incrWordCount("foo");
+    wc.incrWordCount("Foo"); 
+    wc.incrWordCount("bar");
+    wc.incrWordCount("BAR"); 
+    wc.incrWordCount("foo-bar");
+    
+    ASSERT_EQUALS(2, wc.getWordCount("foo"));
+    ASSERT_EQUALS(2, wc.getWordCount("bar"));
+    ASSERT_EQUALS(1, wc.getWordCount("foo-bar"));
+    ASSERT_EQUALS(5, wc.getTotalWords());
+    ASSERT_EQUALS(3, wc.getNumUniqueWords());
 
     std::cout << "All tests passed!" << std::endl;
     return 0;
